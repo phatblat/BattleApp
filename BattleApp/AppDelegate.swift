eb25b4bb7ab7e0bc7dc7b2dc6cf339f10da8d89d
Ben Chatelain <ben@octop.ad>
2018-04-21 17:13:06 -0600

    🎉 Add single screen template

---
+++ BattleApp/AppDelegate.swift
@@ -0,0 +1,28 @@
+//
+//  AppDelegate.swift
+//  BattleApp
+//
+//  Created by Ben Chatelain on 4/21/18.
+//  Copyright © 2018 Jack Chatelain. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+
+    func applicationWillResignActive(_ application: UIApplication) {}
+
+    func applicationDidEnterBackground(_ application: UIApplication) {}
+
+    func applicationWillEnterForeground(_ application: UIApplication) {}
+
+    func applicationDidBecomeActive(_ application: UIApplication) {}
+
+    func applicationWillTerminate(_ application: UIApplication) {}
+}
---
+++ BattleApp/ViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ViewController.swift
+//  BattleApp
+//
+//  Created by Ben Chatelain on 4/21/18.
+//  Copyright © 2018 Jack Chatelain. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
